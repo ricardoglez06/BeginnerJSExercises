6c84b0e7a8e548d6fefc5d446f5b1f88776f7865
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-18 15:18:26 -0600

    The file index2.html and secondProblems.js were added, also I also completed the code for the problem 1.1

---
+++ firstProblems.js
@@ -0,0 +1,16 @@
+// PROBLEMA 1
+// let randomNumber = Math.floor(Math.random()*100);
+// console.log(randomNumber);
+// tryNumber = prompt("Intenta adivinar el numero aleatorio");
+
+// while (tryNumber != randomNumber){
+//     if (tryNumber > randomNumber){
+//         tryNumber = prompt("Try with a lower number");
+//     } else {
+//         tryNumber = prompt("Try with a higher number");
+//     }
+// }
+// alert("Congratulations, you guessed the number correctly");
+// document.write("The random number was ", randomNumber);
+
+// PROBLEMA 2
