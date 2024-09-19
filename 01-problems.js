67094349474c508ff9c50e65b151a8488ccc701f
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-19 08:52:12 -0600

    The problem 1.2 solution was added

--- 01-problems.js
+++ 01-problems.js
@@ -1,7 +1,7 @@
 //PROBLEM 1
 // let randomNumber = Math.floor(Math.random()*100);
 // console.log(randomNumber);
-// tryNumber = prompt("Intenta adivinar el numero aleatorio");
+// tryNumber = prompt("Try to guess the random number");
 
 // while (tryNumber != randomNumber){
 //     if (tryNumber > randomNumber){
@@ -16,3 +16,11 @@
 
 
 // PROBLEM 2
+let amount = prompt("What is the amount to withdraw?");
+if (amount > 50){
+    let bills = Math.floor(amount/50);
+    let remainingMoney = amount % 50;
+    document.write(`You´ll get ${bills} bills of $50 and the remaining money is $${remainingMoney}`) 
+} else {
+    alert("There are not enough funds");
+}
