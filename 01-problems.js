44d1d82dbb6271febf33c73b3acc6092f6c0eff8
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-20 19:51:39 -0600

    The problem 1.7 solution was added

--- 01-problems.js
+++ 01-problems.js
@@ -60,16 +60,89 @@
 
 
 // PROBLEM 6
-let n = prompt("Enter the number you want to calculate its factorial");
-document.write(` ${n} *`);
-let summation = n;
+// let n = prompt("Enter the number you want to calculate its factorial");
+// document.write(` ${n} *`);
+// let summation = n;
 
-for (let i = n-1; i > 0; i--){
-    summation *= i;
-    if (i <= 1){
-        document.write(` ${i}`);
-        break;
-    }
-    document.write(` ${i} *`);
+// for (let i = n-1; i > 0; i--){
+//     summation *= i;
+//     if (i <= 1){
+//         document.write(` ${i}`);
+//         break;
+//     }
+//     document.write(` ${i} *`);
+// }
+// document.write(` = ${summation}`);
+
+
+//PROBLEM 7
+function lenghtConversion(){
+    let option = prompt("1. Cm to M\n 2. M to Km\n\n Choose an option");
+        switch (option){
+            case "1":
+                let cm = prompt("Enter the cm");
+                document.write(`${cm}cm = ${cm/100}m`);
+                break;
+            case "2":
+                let m = prompt("Enter the m");
+                document.write(`${m}m = ${m/1000}km`);
+                break;
+            default:
+                alert("That is not an opton");
+                break;
+        }
 }
-document.write(` = ${summation}`);
+
+function weightConversion(){
+    let option = prompt("1. G to Kg\n 2. Kg to G\n\n Choose an option");
+        switch (option){
+            case "1":
+                let g = prompt("Enter the g");
+                document.write(`${g}g = ${g/1000}kg`);
+                break;
+            case "2":
+                let kg = prompt("Enter the kg");
+                document.write(`${kg}kg = ${kg*1000}g`);
+                break;
+            default:
+                alert("That is not an option");
+        }
+}
+
+function timeConversion(){
+    option = prompt("1. Seconds to Minutes\n 2. Minutes to Hours\n\n Choose an option");
+    let minutes;
+        switch (option){
+            case "1":
+                let seconds = prompt("Enter the seconds");
+                minutes = Math.floor(seconds/60);
+                let secondsRemaining = seconds % 60;
+                document.write(`${seconds} seconds = ${minutes} minutes and ${secondsRemaining} seconds`);
+                break;
+            case "2":
+                minutes = prompt("Enter the minutes");
+                let hours = Math.floor(minutes/60);
+                let minutesRemaining = minutes % 60;
+                document.write(`${minutes} minutes = ${hours} hours and ${minutesRemaining} minutes`);
+                break;
+            default:
+                alert("That is not an option");
+                break;
+        }
+}
+
+let menu = prompt("MENU\n 1. Lenght\n 2. Weight\n 3. Time\n\n Choose an option");
+switch (menu) {
+    case "1":
+        lenghtConversion();
+        break;
+    case "2":
+        weightConversion();
+        break;
+    case "3":
+        timeConversion();
+        break;
+    default:
+        alert("That is not an option");
+        break;
+}
