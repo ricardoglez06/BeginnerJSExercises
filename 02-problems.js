c50dfa684a52213928efc6c16020549766fe0672
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-21 20:56:47 -0600

    The exercise 2.2 solution was added

--- 02-problems.js
+++ 02-problems.js
@@ -1,7 +1,36 @@
 //EJERCICIO 01
-function findMax(a,b){
-    return (a>b) ? a : b;
+// function findMax(a,b){
+//     return (a>b) ? a : b;
+// }
+
+// let max = findMax(12,5);
+// console.log(max);
+
+//EJERCICIO 02
+/**
+ * Name: width x height
+ * 8K 7680 x 4320
+ * 4K 3840 x 2160
+ * WQHD 2560 x 1440
+ * FHD 1920 x 1080
+ * HD 1280 x 720
+ */
+
+function resolutionName(width,height){
+    if (width >= 7680 && height >= 4320){
+        return "8K";
+    } else if (width >= 3840 && height >= 2160){
+        return "4K";
+    } else if (width >= 2560 && height >= 1440){
+        return "WQHD";
+    } else if (width >= 1920 && height >= 1080){
+        return "FHD";
+    } else if (width >= 1280 && height >= 720){
+        return "HD";
+    } else {
+        return false;
+    }
 }
 
-let max = findMax(12,5);
-console.log(max);
+let name = resolutionName(1366,768);
+console.log(name);
