b777d730e49e93d4584907b32f5b29a47d3e7620
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-26 10:54:45 -0600

    The problem 3.2 solution was added

--- 03-problems.js
+++ 03-problems.js
@@ -1,8 +1,21 @@
 // PROBLEM 1
-let total = 0;
-for (let i=0; i<15; i++){
-    let wages = prompt(`Enter the wage of the teacher #${i+1}`);
-    total += parseInt(wages);
+// let total = 0;
+// for (let i=0; i<15; i++){
+//     let wages = prompt(`Enter the wage of the teacher #${i+1}`);
+//     total += parseInt(wages);
+// }
+
+// console.log(`The total to be paid is $${total}`);
+
+
+// PROBLEM 2
+let n = prompt("How many integers do you want to add");
+let summation = 0;
+
+for (let i=0; i<n; i++){
+    let integer;
+    integer = parseInt(prompt("Enter a integer"));
+    summation += integer;
 }
 
-console.log(`The total to be paid is $${total}`);
+console.log(`The result is ${summation}`);
