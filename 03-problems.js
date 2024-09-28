175b7e037c5ae149072836660ea0acf56b222813
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-27 19:41:44 -0600

    The problem 3.3 and 3.4 solutions were added

--- 03-problems.js
+++ 03-problems.js
@@ -9,13 +9,36 @@
 
 
 // PROBLEM 2
-let n = prompt("How many integers do you want to add");
-let summation = 0;
+// let n = prompt("How many integers do you want to add");
+// let summation = 0;
 
-for (let i=0; i<n; i++){
-    let integer;
-    integer = parseInt(prompt("Enter a integer"));
-    summation += integer;
+// for (let i=0; i<n; i++){
+//     let integer;
+//     integer = parseInt(prompt("Enter a integer"));
+//     summation += integer;
+// }
+
+// console.log(`The result is ${summation}`);
+
+
+// PROBLEM 3
+// let total = 0;
+// for (let i=0; i<5; i++){
+//     let number = parseInt(prompt("Enter a number"));
+//     let square = Math.pow(number,2);
+//     total += square;
+// }
+
+// console.log(`The result is ${total}`);
+
+// PROBLEM 4
+let mainNumber = 0;
+let secondaryNumber = 1;
+let aux;
+
+for (let i=0; i<50; i++){
+    console.log(`${numMain}, `);
+    aux = numMain + secondaryNumber;
+    secondaryNumber = numMain;
+    numMain = aux;
 }
-
-console.log(`The result is ${summation}`);
