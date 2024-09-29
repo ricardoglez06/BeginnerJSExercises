ec412cfbdd626b97f43e3db9a458fc4a2bdd9a1c
Ricardoo <angelricardoglezsoto@gmail.com>
2024-09-28 20:04:48 -0600

    The problem 2.9 solution was added

--- 02-problems.js
+++ 02-problems.js
@@ -90,32 +90,61 @@
 
 
 // EXERCISE 08
-let array = [{
-    id: 1,
-    name: "Nicolas",
-},{
-    id: 2,
-    name: "Felipe",
-},{
-    id: 3,
-    name: "Chanchito",
-}];
+// let array = [{
+//     id: 1,
+//     name: "Nicolas",
+// },{
+//     id: 2,
+//     name: "Felipe",
+// },{
+//     id: 3,
+//     name: "Chanchito",
+// }];
 
-/** RESULT EXPECTED (pairs = array of arrays)
- */ let expectedPairs = [
-    [1, { id: 1, name: "Nicolas"}],
-    [2, { id: 2, name: "Felipe"}],
-    [3, { id: 3, name: "Chanchito"}],
- ]
+// /** RESULT EXPECTED (pairs = array of arrays)
+//  */ let expectedPairs = [
+//     [1, { id: 1, name: "Nicolas"}],
+//     [2, { id: 2, name: "Felipe"}],
+//     [3, { id: 3, name: "Chanchito"}],
+//  ]
 
- function toPairs(arr) {
-    let pairs = [];
-    for (idx in arr){
-        let element = arr[idx];
-        pairs[idx] = [element.id, element];
-    }
-    return pairs;
- }
+//  function toPairs(arr) {
+//     let pairs = [];
+//     for (idx in arr){
+//         let element = arr[idx];
+//         pairs[idx] = [element.id, element];
+//     }
+//     return pairs;
+//  }
 
- let result = toPairs(array);
- console.log(result);
+//  let result = toPairs(array);
+//  console.log(result);
+
+
+// EXERCISE 09
+// let pairs = [
+//     [1, { name: "Nicolas"}],
+//     [2, { name: "Felipe"}],
+//     [3, { name: "Chanchito"}],
+// ];
+
+// /** RESULT EXPECTED
+//  */
+// let array = [
+//     {id: 1, name: "Nicolas",},
+//     {id: 2, name: "Felipe",},
+//     {id: 3, name: "Chanchito",}
+// ];
+
+// function toArray(pairs) {
+//     let array = [];
+//     for (idx in pairs) {
+//         let element = pairs[idx];
+//         array[idx] = element[1];
+//         array[idx].id = element[0];
+//     }
+//     return array;
+// }
+
+// let result = toArray(pairs);
+// console.log(result);
